aae70e98faed44c4ac8487dce893dd56c07ebf62
Brooklyn Zelenka <be.zelenka@gmail.com>
2020-05-10 21:43:50 -0700

    Cleanup

--- library/Control/Monad/Raise/Class.hs
+++ library/Control/Monad/Raise/Class.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE AllowAmbiguousTypes   #-}
 {-# LANGUAGE DefaultSignatures     #-}
 {-# LANGUAGE FlexibleContexts      #-}
@@ -9,14 +7,9 @@
 {-# LANGUAGE TypeApplications      #-}
 {-# LANGUAGE UndecidableInstances  #-}
 
-
-
-{-# LANGUAGE FunctionalDependencies #-}
-
 -- | FIXME
 
-module Control.Monad.Raise.Class (MonadRaise (..), ToOpenUnion (..)) where
-
+module Control.Monad.Raise.Class (MonadRaise (..)) where --, ToOpenUnion (..)) where
 
 import           Control.Monad.Cont
 import           Control.Monad.Catch.Pure
@@ -35,7 +28,6 @@ import qualified Control.Monad.State.Strict as Strict
 import qualified Control.Monad.Writer.Lazy   as Lazy
 import qualified Control.Monad.Writer.Strict as Strict
 
-import Data.Kind
 import           Data.WorldPeace
 
 -- $setup
@@ -47,20 +39,8 @@ import           Data.WorldPeace
 -- >>> import Data.Proxy
 -- >>> import Data.WorldPeace
 
-class ToOpenUnion err (errs :: [Type]) where -- | errs -> err where
-  consistent :: err -> OpenUnion errs
-
-instance IsMember err errs => ToOpenUnion err errs where
-  consistent = openUnionLift
-
-instance Contains inner outer => ToOpenUnion (OpenUnion inner) outer where
-  consistent = relaxOpenUnion
-
-instance ToOpenUnion (OpenUnion errs) errs where
-  consistent a = a
-
 -- | Raise semantics, like a type-directed @MonadThrow@
-class Monad m => MonadRaise err m where -- | m -> err where
+class Monad m => MonadRaise err m where
   -- | Raise an error
   --
   -- The @Proxy@ gives a type hint to the type checker.
@@ -94,11 +74,32 @@ instance MonadRaise errs [] where
 instance MonadRaise errs Maybe where
   raise _ = Nothing
 
-instance ToOpenUnion err errs => MonadRaise err (Either (OpenUnion errs)) where
-  raise = Left . consistent
+instance MonadRaise err (Either err) where
+  raise = Left
 
-instance (Monad m, ToOpenUnion err errs) => MonadRaise err (ExceptT (OpenUnion errs) m) where
-  raise = ExceptT . pure . raise
+instance IsMember err errs => MonadRaise err (Either (OpenUnion errs)) where
+  raise = Left . openUnionLift
+
+instance Contains inner outer
+  => MonadRaise (OpenUnion inner) (Either (OpenUnion outer)) where
+    raise = Left . relaxOpenUnion
+ 
+instance Monad m => MonadRaise err (ExceptT err m) where
+  raise = ExceptT . pure . Left
+
+instance
+  ( Monad m
+  , IsMember err errs
+  )
+  => MonadRaise err (ExceptT (OpenUnion errs) m) where
+    raise = ExceptT . pure . Left . openUnionLift
+
+instance
+  ( Monad m
+  , Contains inner outer
+  )
+  => MonadRaise (OpenUnion inner) (ExceptT (OpenUnion outer) m) where
+    raise = ExceptT . pure . Left . relaxOpenUnion
 
 instance MonadRaise errs m => MonadRaise errs (MaybeT m) where
   raise = lift . raise
@@ -121,14 +122,30 @@ instance MonadRaise errs m => MonadRaise errs (Lazy.StateT s m) where
 instance MonadRaise errs m => MonadRaise errs (Strict.StateT s m) where
   raise = lift . raise
 
-instance (Monoid w, MonadRaise errs m) => MonadRaise errs (Lazy.WriterT w m) where
-  raise = lift . raise
+instance
+  ( MonadRaise errs m
+  , Monoid w
+  )
+  => MonadRaise errs (Lazy.WriterT w m) where
+    raise = lift . raise
 
-instance (Monoid w, MonadRaise errs m) => MonadRaise errs (Strict.WriterT w m) where
-  raise = lift . raise
+instance
+  ( MonadRaise errs m
+  , Monoid w
+  )
+  => MonadRaise errs (Strict.WriterT w m) where
+    raise = lift . raise
 
-instance (MonadRaise errs m, Monoid w) => MonadRaise errs (Lazy.RWST r w s m) where
-  raise = lift . raise
+instance
+  ( MonadRaise errs m
+  , Monoid w
+  )
+  => MonadRaise errs (Lazy.RWST r w s m) where
+    raise = lift . raise
 
-instance (MonadRaise errs m, Monoid w) => MonadRaise errs (Strict.RWST r w s m) where
-  raise = lift . raise
+instance
+  ( MonadRaise errs m
+  , Monoid w
+  )
+  => MonadRaise errs (Strict.RWST r w s m) where
+    raise = lift . raise
