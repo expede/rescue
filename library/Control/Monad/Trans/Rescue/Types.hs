f5737ea94f87b74c60bb605840131acbdf33e6db
Brooklyn Zelenka <be.zelenka@gmail.com>
2020-05-16 17:36:50 -0700

    Fix MonadCatch instance

--- library/Control/Monad/Trans/Rescue/Types.hs
+++ library/Control/Monad/Trans/Rescue/Types.hs
@@ -1,106 +1,106 @@
 {-# LANGUAGE ApplicativeDo         #-}
 {-# LANGUAGE FlexibleInstances     #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE LambdaCase            #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeSynonymInstances  #-}
 {-# LANGUAGE UndecidableInstances  #-}
 
+
+
+---
+{-# LANGUAGE AllowAmbiguousTypes   #-}
+---
+
 -- | FIXME
 
-module Control.Monad.Trans.Rescue.Types () where
---   ( RescueT (..)
---   , Rescue
---   , runRescue
---   ) where
+module Control.Monad.Trans.Rescue.Types
+  ( RescueT (..)
+  , Rescue
+  , runRescue
+  ) where
 
--- import           Control.Monad.Catch
--- import           Control.Monad.Cont
--- import           Control.Monad.Fix
--- import           Control.Monad.Rescue
+import           Control.Monad.Catch
+import           Control.Monad.Cont
+import           Control.Monad.Fix
+import           Control.Monad.Rescue
 
--- import           Data.Functor.Identity
--- import           Data.WorldPeace
+import           Data.Functor.Identity
+import           Data.WorldPeace
 
--- -- | Add type-directed error handling abilities to a 'Monad'.
--- newtype RescueT errs m a
---   = RescueT { runRescueT :: m (Either (OpenUnion errs) a) }
+-- | Add type-directed error handling abilities to a 'Monad'.
+newtype RescueT errs m a
+  = RescueT { runRescueT :: m (Either (OpenUnion errs) a) }
 
--- -- | A specialized version of 'RescueT'.
--- type Rescue errs = RescueT errs Identity
+-- | A specialized version of 'RescueT'.
+type Rescue errs = RescueT errs Identity
 
--- runRescue :: Rescue errs a -> Either (OpenUnion errs) a
--- runRescue = runIdentity . runRescueT
+runRescue :: Rescue errs a -> Either (OpenUnion errs) a
+runRescue = runIdentity . runRescueT
 
--- instance Eq (m (Either (OpenUnion errs) a)) => Eq (RescueT errs m a) where
---   RescueT a == RescueT b = a == b
+instance Eq (m (Either (OpenUnion errs) a)) => Eq (RescueT errs m a) where
+  RescueT a == RescueT b = a == b
 
--- instance Show (m (Either (OpenUnion errs) a)) => Show (RescueT errs m a) where
---   show (RescueT inner) = "RescueT (" <> show inner <> ")"
+instance Show (m (Either (OpenUnion errs) a)) => Show (RescueT errs m a) where
+  show (RescueT inner) = "RescueT (" <> show inner <> ")"
 
--- instance Functor m => Functor (RescueT errs m) where
---   fmap f (RescueT inner) = RescueT $ fmap (fmap f) inner
+ -- TODO instance Display
 
--- instance Applicative m => Applicative (RescueT errs m) where
---   pure = RescueT . pure . pure
---   (RescueT fs) <*> (RescueT xs) = RescueT $ do
---     innerFs <- fs
---     innerXs <- xs
---     return (innerFs <*> innerXs)
+instance Functor m => Functor (RescueT errs m) where
+  fmap f (RescueT inner) = RescueT $ fmap (fmap f) inner
 
--- instance Monad m => Monad (RescueT errs m) where
---   RescueT action >>= k = RescueT $ action >>= \case
---     Left  err -> return (Left err)
---     Right val -> runRescueT (k val)
+instance Applicative m => Applicative (RescueT errs m) where
+  pure = RescueT . pure . pure
+  (RescueT fs) <*> (RescueT xs) = RescueT $ do
+    innerFs <- fs
+    innerXs <- xs
+    return (innerFs <*> innerXs)
 
--- instance MonadTrans (RescueT errs) where
---   lift action = RescueT (Right <$> action)
+instance Monad m => Monad (RescueT errs m) where
+  RescueT action >>= k = RescueT $ action >>= \case
+    Left  err -> return (Left err)
+    Right val -> runRescueT (k val)
 
--- instance MonadIO m => MonadIO (RescueT errs m) where
---   liftIO io = RescueT $ do
---     action <- liftIO io
---     return (Right action)
+instance MonadTrans (RescueT errs) where
+  lift action = RescueT (Right <$> action)
 
--- instance MonadFix m => MonadFix (RescueT errs m) where
---   mfix f = RescueT . mfix $ \a ->
---     runRescueT . f $ case a of
---        Right r -> r
---        _       -> error "Empty mfix argument" -- absurd
+instance MonadIO m => MonadIO (RescueT errs m) where
+  liftIO io = RescueT $ do
+    action <- liftIO io
+    return (Right action)
 
--- instance Foldable m => Foldable (RescueT errs m) where
---   foldMap f (RescueT m) = foldMap (foldMapEither f) m where
---     foldMapEither g (Right a) = g a
---     foldMapEither _ (Left _) = mempty
+instance MonadFix m => MonadFix (RescueT errs m) where
+  mfix f = RescueT . mfix $ \a ->
+    runRescueT . f $ case a of
+       Right r -> r
+       _       -> error "Empty mfix argument" -- absurd
 
--- instance (Monad m, Traversable m) => Traversable (RescueT errs m) where
---   traverse f (RescueT m) = RescueT <$> traverse (traverseEither f) m
---     where
---       traverseEither g (Right val) = Right <$> g val
---       traverseEither _ (Left  err) = pure (Left err)
+instance Foldable m => Foldable (RescueT errs m) where
+  foldMap f (RescueT m) = foldMap (foldMapEither f) m where
+    foldMapEither g (Right a) = g a
+    foldMapEither _ (Left _)  = mempty
 
--- instance (IsMember err errs, Monad m) => MonadRaise err (RescueT errs m) where
---   raise err = RescueT . pure . Left $ openUnionLift err
+instance (Monad m, Traversable m) => Traversable (RescueT errs m) where
+  traverse f (RescueT m) = RescueT <$> traverse (traverseEither f) m
+    where
+      traverseEither g (Right val) = Right <$> g val
+      traverseEither _ (Left  err) = pure (Left err)
 
--- instance Monad m => MonadRescue errs (RescueT errs m) where
---   attempt (RescueT action) = RescueT (Right <$> action)
+instance Monad m => MonadRaise (RescueT errs m) where
+  type Errors (RescueT errs m) = errs
+  raise err = RescueT . pure $ raise err
 
--- instance (IsMember SomeException errs, Monad m) => MonadThrow (RescueT errs m) where
---   throwM = RescueT . pure . Left . openUnionLift . toException
+instance Monad m => MonadRescue (RescueT errs m) where
+  attempt (RescueT action) = RescueT (Right <$> action)
 
--- -- -- instance forall errs err m . (MonadRescue errs (RescueT errs m), IsMember SomeException errs, IsMember err errs, Monad m) => MonadCatch (RescueT errs m) where
--- -- instance forall err errs m . (Exception err, MonadRescue errs (RescueT errs m), IsMember err errs, IsMember SomeException errs, Monad m) => MonadCatch (RescueT errs m) where
--- --   catch action handler = -- (handler :: err -> RescueT errs m a) =
--- --     attempt action >>= \case
--- --       Right val  -> pure val
--- --       Left  errs ->
--- --         case openUnionMatch errs of -- openUnionHandle raise withSomeException errs
--- --           Nothing -> undefined -- raise errs
--- --           Just exc -> withSomeException exc
+instance (IsMember SomeException errs, Monad m) => MonadThrow (RescueT errs m) where
+  throwM = RescueT . pure . Left . openUnionLift . toException
 
--- --     where
--- --       withSomeException :: SomeException -> RescueT errs m a
--- --       withSomeException err =
--- --         case fromException err of
--- --           Nothing   -> raise err
--- --           Just err' -> handler err'
+instance (IsMember SomeException errs, Contains errs errs, Monad m) => MonadCatch (RescueT errs m) where
+  catch action handler =
+    rescue action $ \errs ->
+      case openUnionMatch errs of
+        Nothing  -> raise errs
+        Just err -> maybe (raise err) handler $ fromException err
