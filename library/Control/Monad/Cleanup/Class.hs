2ce344b7d1f51c08bcef4e69ac82e8be44127cbf
Brooklyn Zelenka <be.zelenka@gmail.com>
2020-05-20 21:36:53 -0700

    Very WIP

--- library/Control/Monad/Cleanup/Class.hs
+++ library/Control/Monad/Cleanup/Class.hs
@@ -9,6 +9,8 @@ module Control.Monad.Cleanup.Class where
 
 import           Data.Functor
 
+import           Control.Monad.IO.Unlift
+
 import           Control.Exception
 import           Control.Monad.Rescue
 import           Data.WorldPeace
@@ -97,6 +99,26 @@ instance Contains errs errs => MonadCleanup (Either (OpenUnion errs)) where
         _ <- onOk resource output
         return output
 
+-- unliftedCleanup
+--   :: MonadUnliftIO m
+--   => m resource                                        -- ^ acquire some resource
+--   -> (resource -> OpenUnion (Errors m) -> m _ignored1) -- ^ cleanup, some exception thrown
+--   -> (resource -> a                    -> m _ignored2) -- ^ cleanup, no exception thrown. The exception will be rethrown
+--   -> (resource ->                         m a)         -- ^ inner action to perform with the resource
+--   -> m a
+
+-- unliftedCleanup acquire onErr onOk action =
+--   withRunInIO $ \runInIO -> do
+--     resource <- acquire
+--     attempt (action resource) >>= \case
+--       Left err -> do
+--         _ <- onErr resource err
+--         raise err
+
+--       Right output -> do
+--         _ <- onOk resource output
+--         return output
+
 liftedCleanup
   :: ( MonadTrans   t
      , MonadCleanup m
@@ -111,6 +133,6 @@ liftedCleanup res onErr onOk action = do
   inner <- res
   lift $ cleanup (pure inner) onErr onOk action
 
-finally :: MonadCleanup m => m a -> m b -> m a
+-- finally :: MonadCleanup m => m a -> m b -> m a
 
-cleanRetry :: MonadCleanup m => Nat -> m a -> m a
+-- cleanRetry :: MonadCleanup m => Nat -> m a -> m a
