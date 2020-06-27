c5c336e346ceea1f047a3b4ad966b5eee838b994
Brooklyn Zelenka <be.zelenka@gmail.com>
2020-06-27 15:38:24 -0700

    Remove unused instance, add docs

--- library/Data/WorldPeace/Subset/Class/Internal.hs
+++ library/Data/WorldPeace/Subset/Class/Internal.hs
@@ -4,26 +4,19 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE UndecidableInstances  #-}
 
--- | FIXME docs
-
 module Data.WorldPeace.Subset.Class.Internal (Subset' (..)) where
 
 import           Data.Kind
 import           Data.Proxy
 import           Data.WorldPeace
 
--- FIXME docs
--- NOTE 'flag :: Bool' is a hack around the overlapping instances problem
+-- | Hack around overlapping instances to check subsets
+--   of bare values or 'OpenUnion's automatically
 class Subset' (flag :: Bool) (err :: Type) (errs :: [Type]) where
   include' :: Proxy flag -> err -> OpenUnion errs
 
 instance Contains err errs => Subset' 'True (OpenUnion err) errs where
   include' _ = relaxOpenUnion
-
--- FIXME Seems to work without this, but mayb ethis will make compiles faster?
---       But also maybe makes it worse? Needs futher investigation!
-instance Subset' 'False (OpenUnion errs) errs where
-  include' _ a = a
 
 instance IsMember err errs => Subset' 'False err errs where
   include' _ = openUnionLift
