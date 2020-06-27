97bfa0573f17329dae1b4e6a79e1f75e60267826
Brooklyn Zelenka <be.zelenka@gmail.com>
2020-06-27 15:59:53 -0700

    Docs

--- library/Data/WorldPeace/Subset/Class.hs
+++ library/Data/WorldPeace/Subset/Class.hs
@@ -1,10 +1,9 @@
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeApplications      #-}
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE UndecidableInstances  #-}
-
--- | FIXME docs
 
 module Data.WorldPeace.Subset.Class (Subset (..)) where
 
@@ -13,11 +12,14 @@ import           Data.WorldPeace
 import           Data.WorldPeace.IsOpenUnion.Family
 import           Data.WorldPeace.Subset.Class.Internal
 
--- FIXME docs
-
+-- | State that some type @err@ is a subset of @errs@
+--
+--   In essence, this is used to avoid having to specially lift
+--   bare values to an 'OpenUnion'. We can treat all types
+--   and 'OpenUnion's the same.
 class Subset err errs where
-  include :: err -> errs -- FIXME heavily inline
+  include :: err -> errs
 
-instance (IsOpenUnion err ~ flag, Subset' flag err errs)
-  => Subset err (OpenUnion errs) where
-    include = include' (Proxy :: Proxy flag)
+instance (IsOpenUnion err ~ flag, Subset' flag err errs) => Subset err (OpenUnion errs) where
+  include = include' (Proxy @flag)
+  {- INLINE include -}
